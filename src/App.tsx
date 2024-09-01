c0d8ebfa0bcfbfc5f0c772e594f562690325e1f3
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-09-01 22:51:05 +0900

    [feat] 라우터 추가
    샘플을 쉽게 관리하기 위해 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,13 @@
-import './App.css';
-
-import {Todo, TodoTemplate} from "./component/todo/TodoTemplate";
-import {useCallback, useReducer, useRef, useState} from "react";
-import {todoReducer} from "./component/todo/todoReducer";
-
-const createBulkTodos = () => {
-    const array: Todo[] = [];
-    for(let i = 1; i <= 2500; i++) {
-        array.push({id: i, text: `할 일 ${i}`, checked: false});
-    }
-    return array;
-};
+import {Route, Routes} from "react-router-dom";
+import Main from "./pages/Main";
+import TodoPage from "./pages/TodoPage";
 
 const App = () => {
-
-    const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
-
-    const nextId = useRef(2501);
-
-    const onInsert = useCallback((text: string) => {
-        if(text) {
-            const todo = {id: nextId.current, text: text, checked: false};
-            dispatch({type: 'INSERT', todo});
-            nextId.current += 1;
-        }
-    },[]);
-
-    const onRemove = useCallback((id: number) => {
-        dispatch({type: 'REMOVE', id});
-    },[]);
-
-    const onToggle = useCallback((id: number) => {
-        dispatch({type: 'TOGGLE', id})
-    },[]);
-
     return (
-        <div className="App">
-            <TodoTemplate todos={todos} onInsert={onInsert} onRemove={onRemove} onToggle={onToggle}/>
-        </div>
+        <Routes>
+            <Route path={`/`} element={<Main/>}/>
+            <Route path={`/todo`} element={<TodoPage/>}/>
+        </Routes>
     );
 }
 
