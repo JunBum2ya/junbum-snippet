8300b0efe64362582f644649fa993b17187e620d
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-09-08 22:04:56 +0900

    [feat] html 부분 캡처 기능 구현
    html 부분 캡처 기능 구현

--- src/component/htmlCapture/HtmlCaptureTemplate.tsx
+++ src/component/htmlCapture/HtmlCaptureTemplate.tsx
@@ -1,44 +1,52 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import './HtmlCapture.scss';
-import {useLocation} from "react-router-dom";
+import html2canvas from "html2canvas";
 
-export const HtmlCaptureTemplate: React.FC<HtmlCaptureTemplateProps> = ({children}) => {
+export const HtmlCaptureTemplate: React.FC<HtmlCaptureTemplateProps> = ({children, onCapture}) => {
+
+    const htmlCaptureRef = useRef<HTMLDivElement>(null);
 
     const [status, setStatus] = useState<CaptureStatus>('DORMANCY');
     const [start, setStart] = useState<LocationType>({x: 0, y: 0});
     const [end, setEnd] = useState<LocationType>({x: 0, y: 0});
 
-    const handleClickTemplate = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    const handleMouseClick = useCallback(async (e: React.MouseEvent<HTMLDivElement>) => {
         if (status === 'DORMANCY') {
             setStatus('PROGRESS');
             setStart({x: e.clientX, y: e.clientY});
         } else {
-            setStatus('DORMANCY');
+            if (htmlCaptureRef.current) {
+                await html2canvas(htmlCaptureRef.current, {
+                    x: start.x,
+                    y: start.y,
+                    width: end.x - start.x,
+                    height: end.y - start.y
+                }).then(canvas => {
+                    if (onCapture) {
+                        onCapture(canvas);
+                    }
+                });
+                setStatus('DORMANCY');
+            }
         }
-    }, [status]);
+    }, [status, onCapture]);
 
-    const handleMoveTemplate = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    const handleMouseMove = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
         if (status === 'PROGRESS') {
             setEnd({x: e.clientX, y: e.clientY});
         }
     }, [status]);
 
     return (
-        <div className={`capture_template ${status}`} onClick={handleClickTemplate} onMouseMove={handleMoveTemplate}>
-            {status === 'PROGRESS' && <HtmlCaptureArea top={start.y} left={start.x} width={end.x} height={end.y}/>}
+        <div ref={htmlCaptureRef} className={`capture_template ${status}`} onClick={handleMouseClick} onMouseMove={handleMouseMove}>
+            {status === 'PROGRESS' && <div className={'capture_area'} style={{
+                top: `${start.y}px`,
+                left: `${start.x}px`,
+                width: `${end.x - start.x}px`,
+                height: `${end.y - start.y}px`
+            }}></div>}
             {children}
         </div>
-    );
-};
-
-const HtmlCaptureArea = (props: HtmlCaptureAreaProps) => {
-    return (
-        <div className={'capture_area'} style={{
-            top: `${props.top}px`,
-            left: `${props.left}px`,
-            width: `${props.width - props.left}px`,
-            height: `${props.height - props.top}px`
-        }}></div>
     );
 };
 
@@ -51,11 +59,5 @@ type LocationType = {
 
 type HtmlCaptureTemplateProps = {
     children: React.ReactNode;
-};
-
-type HtmlCaptureAreaProps = {
-    top: number;
-    left: number;
-    width: number;
-    height: number;
+    onCapture?: (e: HTMLCanvasElement) => void;
 };
