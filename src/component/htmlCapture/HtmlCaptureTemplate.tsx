89caf8f2536a7d49201a4a0268f68ce6dca8f2f0
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-09-10 21:34:14 +0900

    [feat] 캡처 영역 보완
    왼쪽이나 위쪽으로 이동해도 처리가능하도록 구현

--- src/component/htmlCapture/HtmlCaptureTemplate.tsx
+++ src/component/htmlCapture/HtmlCaptureTemplate.tsx
@@ -6,17 +6,11 @@ const captureReducer = (state: CaptureState, action: CaptureAction): CaptureStat
     const {phase, x, y} = action;
     switch (phase) {
         case "START":
-            return {status: 'PROGRESS', top: y, left: x, width: 0, height: 0};
+            return {status: "PROGRESS", startX: x, startY: y, endX: x, endY: y};
         case "PROGRESS":
-            return {
-                status: 'PROGRESS',
-                top: Math.min(state.top, y),
-                left: Math.min(state.left, x),
-                width: Math.abs(x - state.left),
-                height: Math.abs(y - state.top)
-            };
+            return {status: "PROGRESS", startX: state.startX, startY: state.startY, endX: x, endY: y}
         case "END":
-            return {status: 'DORMANCY', top: 0, left: 0, width: 0, height: 0};
+            return {status: "DORMANCY", startX: 0, startY: 0, endX: 0, endY: 0};
         default:
             return state;
     }
@@ -24,7 +18,7 @@ const captureReducer = (state: CaptureState, action: CaptureAction): CaptureStat
 
 export const HtmlCaptureTemplate: React.FC<HtmlCaptureTemplateProps> = ({children, onCapture}) => {
 
-    const [state, dispatch] = useReducer(captureReducer, {status: "DORMANCY", top: 0, left: 0, width: 0, height: 0});
+    const [state, dispatch] = useReducer(captureReducer, {status: "DORMANCY", startX: 0, startY: 0, endX: 0, endY: 0});
 
     const htmlCaptureRef = useRef<HTMLDivElement>(null);
 
@@ -34,10 +28,10 @@ export const HtmlCaptureTemplate: React.FC<HtmlCaptureTemplateProps> = ({childre
         } else {
             if (htmlCaptureRef.current) {
                 const canvas = await html2canvas(htmlCaptureRef.current, {
-                    x: state.left,
-                    y: state.top,
-                    width: state.width,
-                    height: state.height
+                    x: Math.min(state.startX, state.endX),
+                    y: Math.min(state.startY, state.endY),
+                    width: Math.abs(state.endX - state.startX),
+                    height: Math.abs(state.endY - state.startY)
                 });
                 if (onCapture) {
                     onCapture(canvas);
@@ -57,10 +51,10 @@ export const HtmlCaptureTemplate: React.FC<HtmlCaptureTemplateProps> = ({childre
         <div ref={htmlCaptureRef} className={`capture_template ${state.status}`} onClick={handleMouseClick}
              onMouseMove={handleMouseMove}>
             {state.status === 'PROGRESS' && <div className={'capture_area'} style={{
-                top: `${state.top}px`,
-                left: `${state.left}px`,
-                width: `${state.width}px`,
-                height: `${state.height}px`
+                top: `${Math.min(state.startY, state.endY)}px`,
+                left: `${Math.min(state.startX, state.endX)}px`,
+                width: `${Math.abs(state.endX - state.startX)}px`,
+                height: `${Math.abs(state.endY - state.startY)}px`
             }}></div>}
             {children}
         </div>
@@ -73,12 +67,12 @@ type HtmlCaptureTemplateProps = {
 };
 
 type CaptureState = {
-    status: 'DORMANCY' | 'PROGRESS'
-    top: number;
-    left: number;
-    width: number;
-    height: number;
-};
+    status: 'DORMANCY' | 'PROGRESS';
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
+}
 
 type CaptureAction = {
     phase: 'START' | 'PROGRESS' | 'END';
